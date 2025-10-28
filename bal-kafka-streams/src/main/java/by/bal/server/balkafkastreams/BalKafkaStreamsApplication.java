9e7371ee07164cfa812e4fbaecb761aea80c3a16
Pavel Bal <balpoi000@gmail.com>
2025-10-28 10:19:34 +0300

    commit

---
+++ bal-kafka-streams/src/main/java/by/bal/server/balkafkastreams/BalKafkaStreamsApplication.java
@@ -0,0 +1,13 @@
+package by.bal.server.balkafkastreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalKafkaStreamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BalKafkaStreamsApplication.class, args);
+    }
+
+}
---
+++ bal-kafka-streams/src/test/java/by/bal/server/balkafkastreams/BalKafkaStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bal.server.balkafkastreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalKafkaStreamsApplicationTests {
+
+    // @Test
+    // void contextLoads() {
+    // }
+
+}
